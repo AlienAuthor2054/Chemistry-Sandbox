e64edd66891654f9848e575c70839752cf1947ed
AlienAuthor2054 <200651229+AlienAuthor2054@users.noreply.github.com>
2025-03-14 00:54:14 +0400

    Remove unused _ready method in ui.gd

--- ui/ui.gd
+++ ui/ui.gd
@@ -2,10 +2,6 @@ extends CanvasLayer
 
 @onready var zoom_value: float = 1000 / get_viewport().get_visible_rect().size.y
 
-func _ready():
-	return
-	scale = Vector2(zoom_value, zoom_value)
-
 func _on_temperature_button_pressed(velocity_factor: float) -> void:
 	get_tree().call_group("atoms", "multiply_velocity", sqrt(velocity_factor))
 	$"..".external_change_applied.emit()
