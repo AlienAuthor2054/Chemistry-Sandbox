2d6a8e19010fc566959d6050d6b16a7c82216c5a
AlienAuthor2054 <200651229+AlienAuthor2054@users.noreply.github.com>
2025-03-15 01:19:49 +0400

    Fix key combustion reaction not happening
    - Fixes regression from commit 26f0ab2 where HOO is not formed from initial combustion reaction CH4 + O2.
    - Adjusts bond energy calculations to:
       - Strengthen the H-O bond over H-C
       - Strengthen the O-O bond over O=O
    - Also reduces bond visual width to compensate for stronger H-O bond.

--- chemistry/atom/bond/bond.gd
+++ chemistry/atom/bond/bond.gd
@@ -15,12 +15,12 @@ static func get_energy(atom1: Atom, atom2: Atom, order: int) -> float:
 	@warning_ignore("shadowed_variable")
 	var energy: float = 100
 	# Electronegativity difference -> Ionic character (strengthens)
-	energy += 40 * (absf(atom1.electronegativity - atom2.electronegativity) ** 2)
+	energy += 70 * (absf(atom1.electronegativity - atom2.electronegativity) ** 2)
 	# Bond order -> Bonded electrons (strengthens)
-	energy *= [1.0, 1.8, 2.4][order - 1]
+	energy *= [1.0, 1.7, 2.3][order - 1]
 	# Unbonded electron repulsion (weakens)
 	# TODO: Consider external bonds. With that, external changes can affect bond energy, which CBM does not currently support.
-	energy -= 1.0 * (atom1.electrons - order) * (atom2.electrons - order)
+	energy -= 0.8 * (atom1.electrons - order) * (atom2.electrons - order)
 	assert(energy > 0, "Bond energy should be more than zero")
 	return energy
 
@@ -44,7 +44,7 @@ func update_order(new_order: int) -> void:
 	var y_offset := (order - 1) / 2.0
 	for index in range(order):
 		var line: Polygon2D = ATOM_BOND_LINE_SCENE.instantiate()
-		var line_width_scale = energy / order / 64
+		var line_width_scale = energy / order / 100
 		line.position = Vector2(0, (16 + (line_width_scale * 16)) * (index - y_offset))
 		line.scale = Vector2(1, line_width_scale)
 		add_child(line)
