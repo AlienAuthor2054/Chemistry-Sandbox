176c61cc684639bd893e5401d52e19093e981d77
AlienAuthor2054 <200651229+AlienAuthor2054@users.noreply.github.com>
2025-03-23 21:44:56 +0400

    Publish changes to work on another place

--- project/ui/element_hotbar_element.gd
+++ project/ui/element_hotbar_element.gd
@@ -1,9 +1,8 @@
-extends Label
+extends Button
 
 @export var element = 1
 
 @onready var UI := $"/root/Main/UI"
 
-func _on_gui_input(event: InputEvent) -> void:
-	if not (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()): return
+func _on_pressed() -> void:
 	Global.selected_element = element
