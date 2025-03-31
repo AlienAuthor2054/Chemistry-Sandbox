f95f4eb0df633f3376263228696b243abdb6274c
AlienAuthor2054 <200651229+AlienAuthor2054@users.noreply.github.com>
2025-04-01 00:07:53 +0400

    Change selection type from Node2D to Atom
    I originally made it Node2D to account for bond selection in the future.
    
    Bond selection can be part of the "Force form and break bonds while paused" planned feature.
    
    I will most likely be adding bond selection as a separate class, possibly with inheritance.

--- project/simulation/selection.gd
+++ project/simulation/selection.gd
@@ -12,7 +12,7 @@ var box_color: Color
 var box_start := Vector2.ZERO
 var box_end := Vector2.ZERO
 var box_rect := Rect2(0, 0, 0, 0)
-var selected: Dictionary[Node2D, bool] = {}
+var selected: Dictionary[Atom, bool] = {}
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("select_atoms") and not Input.is_action_pressed("control"):
@@ -33,17 +33,16 @@ func _process(_delta: float) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("remove_atom"):
-		for node in selected.duplicate():
-			if node is not Atom: continue
-			remove(node)
-			(node as Atom).remove()
+		for atom in selected.duplicate():
+			remove(atom)
+			atom.remove()
 	if not box_active: return
 	$CollisionShape.position = (box_start + box_end) / 2
 	($CollisionShape.shape as RectangleShape2D).size = box_rect.size
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(atom: Atom) -> void:
 	if not box_active: return
-	add(body) if box_selecting else remove(body)
+	add(atom) if box_selecting else remove(atom)
 
 func _draw() -> void:
 	if not box_active: return
@@ -56,18 +55,18 @@ func update(force: bool = false) -> void:
 	box_rect = Rect2(box_start, box_end - box_start).abs()
 	queue_redraw()
 
-func add(node: Node2D) -> void:
-	if selected.has(node): return
-	node.tree_exiting.connect(remove.bind(node))
-	selected[node] = true
-	node.select()
+func add(atom: Atom) -> void:
+	if selected.has(atom): return
+	atom.tree_exiting.connect(remove.bind(atom))
+	selected[atom] = true
+	atom.select()
 
-func remove(node: Node2D) -> void:
-	if not selected.has(node): return
-	node.tree_exiting.disconnect(remove)
-	selected.erase(node)
-	node.deselect()
+func remove(atom: Atom) -> void:
+	if not selected.has(atom): return
+	atom.tree_exiting.disconnect(remove)
+	selected.erase(atom)
+	atom.deselect()
 	
 func clear() -> void:
-	for node in selected:
-		remove(node)
+	for atom in selected:
+		remove(atom)
