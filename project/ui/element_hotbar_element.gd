89478c3773c626b61afa98313c17d671d1a2bec8
AlienAuthor2054 <200651229+AlienAuthor2054@users.noreply.github.com>
2025-04-06 14:40:54 +0400

    Show element name in hotbar element tooltip

--- project/ui/element_hotbar_element.gd
+++ project/ui/element_hotbar_element.gd
@@ -4,6 +4,9 @@ class_name HotbarItem extends Button
 
 @onready var UI := $"/root/Main/SimulationUI"
 
+func _ready() -> void:
+	tooltip_text = ElementDB.get_data(element).name
+
 func _on_gui_input(event: InputEvent) -> void:
 	if not (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()): return
 	Global.selected_element = element
